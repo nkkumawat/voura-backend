6a83ddb04393c5d7cdfc8aed8e90545bbd462695
nkkumawat <nk0kumawat@gmail.com>
2020-06-20 23:23:38 +0530

    answer make

--- redis.js
+++ redis.js
@@ -2,20 +2,39 @@ const redis = require("ioredis");
 const client = new redis();
 
 module.exports = {
-  saveNewConnection: function(socketId) {
+  saveNewConnection: async function(socketId) {
     client.sadd('connections', socketId);
   },
-  getAllConnections: function(socketId) {
-    client.smembers('connections');
+  getAllConnections: async function() {
+    var data = null;
+    await client.smembers('connections',(err, res) => {
+      data = res;
+    });
+    return data;
+  },
+  removeConnection: async function(socketId) {
+    await client.srem('connections', socketId);
+    await client.smembers(socketId, (err, res) => {
+      console.log(res);
+      res.forEach((room) => {
+        console.log(room)
+        client.del(room);
+      });
+    })
+    await client.del(socketId);
   },
   insertInRoom: async function(roomName, socketId) {
     await client.sadd(roomName, socketId);
+    await client.sadd(socketId, roomName);
   },
   getRoomMembers: async function(roomName) {
-    data = []
+    var data = null;
     await client.smembers(roomName, (err, res) => {
-      data =  res;
+      console.log(err)
+      console.log(res)
+      data = res;
     });
+    console.log("d", data)
     return data;
   },
 }
--- socket.js
+++ socket.js
@@ -3,28 +3,34 @@ var redis = require('./redis');
 module.exports = {
   init: function(io) {
     activeSockets = [];
-    io.sockets.on('connection', function(socket) {
-      console.log(`New connection id: ${socket.id}`)
-      var existingSocket = activeSockets.find(function (existingSocket) { return existingSocket === socket.id; })
-      if (!existingSocket) {
-          activeSockets.push(socket.id);
-          socket.emit("update-user-list", {
-              users: activeSockets.filter(function (existingSocket) { return existingSocket !== socket.id; })
-          });
-          socket.broadcast.emit("update-user-list", {
-              users: [socket.id]
-          });
+    io.sockets.on('connection', async function(socket) {
+      console.log(`New connection, id: ${socket.id}`);
+      await redis.saveNewConnection(socket.id);
+      var activeConnections = await redis.getAllConnections();
+      console.log(activeConnections)
+      if(activeConnections != null && activeConnections.length != 0) {
+        var notMe = activeConnections.filter(function (existingSocket) { return existingSocket !== socket.id; });
+        console.log(notMe);
+        setTimeout(() => {
+        socket.emit("update-user-list", {
+            users: activeConnections.filter(function (existingSocket) { return existingSocket !== socket.id; })
+        });
+      }, 2000);
+        socket.broadcast.emit("update-user-list", {
+            users: activeConnections
+        });
+      } else {
+        await redis.saveNewConnection(socket.id);
       }
-
       socket.on("call-user", function (data) {
-        console.log(data)
-        console.log("calling.................")
+        console.log(`making call to ${data.to} from ${socket.id}` )
           socket.to(data.to).emit("call-made", {
               from: socket.id
           });
       });
 
       socket.on("make-answer", function (data) {
+          console.log(`making answer to ${data.to} from ${socket.id} in room ${data.room}` )
           socket.to(data.to).emit("answer-made", {
               from: socket.id,
               room: data.room
@@ -35,35 +41,21 @@ module.exports = {
         socket.broadcast.emit('message', message);
       });
 
-      socket.on('create or join', async function(room) {
-        console.log('Received request to create or join room ' + room);
+      socket.on('create_or_join', async function(room) {
         var roomMembers = await redis.getRoomMembers(room);
-        console.log(roomMembers)
-        console.log("----------")
-        console.log('Room ' + room + ' now has ' + roomMembers + ' client(s)');
-        if(roomMembers != null) {
-          redis.insertInRoom(room, socket.id);
+        console.log('Room ' + room + ' now has ' + roomMembers);
+        if(roomMembers && roomMembers.length != 0) {
+          await redis.insertInRoom(room, socket.id);
           console.log('Client ID ' + socket.id + ' joined room ' + room);
           io.sockets.in(room).emit('join', room);
           socket.join(room);
           socket.emit('joined', room, socket.id);
           io.sockets.in(room).emit('ready');
         } else {
-          redis.insertInRoom(room, socket.id);
+          await redis.insertInRoom(room, socket.id);
           socket.join(room);
           console.log('Client ID ' + socket.id + ' created room ' + room);
           socket.emit('created', room, socket.id);
-        }
-      });
-
-      socket.on('ipaddr', function() {
-        var ifaces = os.networkInterfaces();
-        for (var dev in ifaces) {
-          ifaces[dev].forEach(function(details) {
-            if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
-              socket.emit('ipaddr', details.address);
-            }
-          });
         }
       });
 
@@ -71,8 +63,9 @@ module.exports = {
         console.log('received bye');
       });
 
-      socket.on('disconnect', function() {
-         console.log('Got disconnect!');
+      socket.on('disconnect', async function() {
+         console.log('Got disconnect!' + socket.id);
+         redis.removeConnection(socket.id);
       });
     });
   }
